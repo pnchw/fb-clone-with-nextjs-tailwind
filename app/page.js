753292aa64e66ac03cad072a727b8808ae0e7116
pnchw <pancheewa.thpm@hotmail.com>
2025-02-25 01:46:27 +0700

    second commit

--- app/page.js
+++ app/page.js
@@ -3,9 +3,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Import authOptions
 import Header from "@/components/Header";
 import Login from "@/components/Login";
-import Sidebar from "@/components/Sidebar";
-import Feed from "@/components/Feed";
-import Widgets from "@/components/Widgets";
 import Main from "@/components/Main";
 
 export default async function Home() {
